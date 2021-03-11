c15b9625ef802f92ab1f9524a7c6ebd8147e6bef
dream-user <sofiasmagluk@MacBook-Air-Sofia.local>
2021-03-11 17:14:36 +0200

    Homework 2
    I created first page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderBlock from "./components/HeaderBlock";
+import LayoutBlock from "./components/LayoutBlock";
+import FooterBlock from "./components/FooterBlock";
 
-function App() {
+import React from "react";
+
+import BackgroundImage from './assets/index.js';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
+    <>
+      <HeaderBlock
+        title="This is title"
+        descr="This is Description!"
+      />
+      <LayoutBlock
+        title="This is title"
+        descr="This is Description!"
+        urlBg={BackgroundImage.FirstBackground}
+        colorBg="#e2e2e2"
+      />
+      <LayoutBlock
+        title="This is title"
+        descr="This is Description!"
+        colorBg="#e2e2e2"
+      />
+      <LayoutBlock
+        title="This is title"
+        descr="This is Description!"
+        urlBg={BackgroundImage.SecondBackground}
+        colorBg="#e2e2e2"
+      />
+      <FooterBlock />
+    </>
   );
 }
 
